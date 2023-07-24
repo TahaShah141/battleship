a67c513cd02c67b85209900c42d11ca1ee020114
Taha Shah <tahashah411@gmail.com>
2023-07-25 04:12:09 +0500

    Webpack config with html and css automation

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
-const path = require('path');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HTMLWebpackPlugin({title: "Project"})
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+        }]
     },
     mode: 'development'
 }
