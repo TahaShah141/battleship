625c2e29269fe1ba316af5fe6cbf178eb4cf4e00
Taha Shah <tahashah411@gmail.com>
2023-07-25 21:49:19 +0500

    Refactored code by introducing ship segments

--- src/gameboard-class.js
+++ src/gameboard-class.js
@@ -2,61 +2,54 @@ const Ship = require('./ship-class')
 
 class Gameboard {
     constructor(n=10) {
-        this.board = this.initBoard(n);
+        this.board = this.initBoard(n)
+        
+        this.tries = [];
+        for (let i = 0; i < n; i++) {
+            this.tries.push([]);
+            for (let j = 0; j < n; j++) {
+                this.tries[i].push(false);
+            }
+        }
+
         this.ships = [];
     }
 
-    initBoard(n) { // " " if not interacted with, true if hit and false if missed
+    initBoard(n) {
         let board = [];
         for (let x = 0; x < n; x++) {
             board.push([]);
             for (let y = 0; y < n; y++) {
-                board[x].push(" ");
+                board[x].push(null);
             }
         }
         return board;
     }
 
-    addShip(length, position, isHorizontal=true) { //assumes legal parameters given
-        let ship = new Ship(length, position.x, position.y, isHorizontal);
-        // let dx = isHorizontal ? 1 : 0
-        // let dy = isHorizontal ? 0 : 1;
-        // for (let i = 0; i < this.ships.length; i++) {
-        //     let s = this.ships[s];
-        //     for (let offset = 0; offset < ship.length; offset++) {
-        //         if (s.collidesWith({x: position.x + offset*dx, y: position.y + offset*dy}) !== -1) return;
-        //     }
-        // }
+    addShip(ship, x, y) {
+        let dx = ship.horizontal ? 1 : 0;
+        let dy = ship.horizontal ? 0 : 1;
+
+        for (let i = 0; i < ship.length; i++) {
+            let X = x+i*dx;
+            let Y = y+i*dy;
+            this.board[X][Y] = ship.addSegment(X, Y);
+        }
+
         this.ships.push(ship);
     }
 
-    receiveAttack(coordinates) { //assumes coordinates are brand new
-        let x = coordinates.x;
-        let y = coordinates.y;
-
-        let shipHit = false;
-
-        for (let i = 0; i < this.ships.length; i++) {
-            let ship = this.ships[i];
-            let offset = ship.collidesWith({x, y}); 
-            if (offset !== -1) {
-                ship.hit(offset);
-                shipHit = true;
-                break;
-            }
-        }
-
-        this.board[x][y] = shipHit;
+    receiveAttack(x, y) {
+        if (this.board[x][y]) this.board[x][y].hit();
+        this.tries[x][y] = true;
     }
 
     allSunk() {
         for (let i = 0; i < this.ships.length; i++) {
-            let ship = this.ships[i];
-            if (!ship.isSunk()) return false;
+            if (!this.ships[i].sunk) return false;
         }
         return true;
     }
 }
 
 module.exports = Gameboard;
-
--- src/ship-class.js
+++ src/ship-class.js
@@ -1,41 +1,54 @@
 class Ship {
-    constructor(length, x, y, horizontal=true) {
+    constructor(length, isHorizontal=true) {
         this.length = length;
-        this.hits = [];
-        for (let i = 0; i < length; i++) this.hits.push(false);
-        this.sunk = false;
-        this.horizontal = horizontal;
-        this.position = {x, y};
-    }
-
-    hit(offset) {
-        this.hits[offset] = true;
+        this.segments = [];
+        this.horizontal = isHorizontal;
+        this.sunk = false;  
     }
 
     isSunk() {
-        for (let i = 0; i < this.hits.length; i++) if (!this.hits[i]) return false;
+        for (let i = 0; i < this.segments.length; i++) if (!this.segments[i].isHit) return false;
         return true;
     }
 
-    collidesWith(coordinates) {
-        let x = coordinates.x;
-        let y = coordinates.y;
+    flip() {
+        this.horizontal = !this.horizontal;
+    }
 
-        let dx = 0;
-        let dy = 0;
+    addSegment(x, y) {
+        let seg = new Segment(this, x, y);
+        this.segments.push(seg);
 
-        let startX = this.position.x;
-        let startY = this.position.y;
+        return seg;
+    }
+}
 
-        if (this.horizontal) dx = 1;
-        else dy = 0;
+class Segment {
+    constructor(ship, x, y) {
+        this.isHit = false;
+        this.x = x;
+        this.y = y;
+        this.parent = ship;
+    }
 
-        for (let i = 0; i < this.length; i++) {
-            if (startX + i*dx === x && startY + i*dy === y) return i;
-        }
-
-        return -1;
+    hit() {
+        this.isHit = true;
+        this.parent.sunk = this.parent.isSunk();
     }
 }
 
 module.exports = Ship;
+
+
+// function test() {
+//     let a = [1, 2, 4, 5, 3, 7, 3];
+
+//     for (let x in a) {
+//         console.log(x);
+//         if (x === 7) return true;
+//     }
+
+//     return false;
+// }
+
+// console.log(test());
